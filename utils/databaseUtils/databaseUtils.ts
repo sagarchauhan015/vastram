9ca6a37772aef02bc3dd655189ca9be812063607
Sagar Chauhan <sagarchauhan015@gmail.com>
2024-01-14 18:46:47 +0530

    added cockroach db setup in databaseUtils

--- utils/databaseUtils/databaseUtils.ts
+++ utils/databaseUtils/databaseUtils.ts
@@ -1,18 +1,33 @@
 import { Sequelize } from "sequelize";
-import mysql2 from 'mysql2';
-import config from "@/config";
+import pg from 'pg';
 
-export const sequelize = new Sequelize(
-    config.db.name,        // Database Name
-    config.db.username,    // Database Username
-    config.db.password,    // Database Passowrd
-    {
-        dialect: 'mysql',
-        dialectModule: mysql2,
-        host: config.db.host,
-        port: config.app.port
-    }
-);
+// import mysql2 from 'mysql2';
+// import config from "@/config";
+
+// export const sequelize = new Sequelize(
+//     config.db.name,        // Database Name
+//     config.db.username,    // Database Username
+//     config.db.password,    // Database Passowrd
+//     {
+//         dialect: 'mysql',
+//         dialectModule: mysql2,
+//         host: config.db.host,
+//         port: config.app.port,
+//         dialectOptions: {
+//             ssl: {
+//                 rejectUnauthorized: true,
+//             },
+//         },
+//         define: {
+//             timestamps: false,
+//         },
+//     }
+// );
+
+const connectionString = process.env.PROD_DATABASE_URL || '';
+export const sequelize = new Sequelize(connectionString,{
+    dialectModule: pg
+});
 
 export async function intializeConnection() {
     try {
